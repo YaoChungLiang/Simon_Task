bf73de2a8e59db437c767a1e7d67070e4e575b57
YaoChungLiang <an4021048@gmail.com>
2021-02-25 16:27:36 -0500

    start page finishe

--- app/src/main/java/com/example/simon_task_java_header/MainActivity.java
+++ app/src/main/java/com/example/simon_task_java_header/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.simon_task_java_header;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,21 @@ public class MainActivity extends AppCompatActivity {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+        TextView testIntro = (TextView)findViewById(R.id.test_intro);
+        testIntro.setText(getString(R.string.test_intro));
+        float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+        testIntro.setTextSize(pixels);
+        Button startButton = (Button)findViewById(R.id.start_button);
+        startButton.setText(getString(R.string.start_button));
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+
+    }
+
 
 
 }
